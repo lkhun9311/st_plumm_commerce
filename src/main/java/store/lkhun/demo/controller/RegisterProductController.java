2a0eb86d0c78761010422643cd4bd5bf8e40a8ad
lkhun9311 <lkhun9311@naver.com>
2023-06-07 22:43:22 +0900

    Add: 상품 CRUD 관련 컨트롤러

--- src/main/java/store/lkhun/demo/controller/RegisterProductController.java
+++ src/main/java/store/lkhun/demo/controller/RegisterProductController.java
@@ -2,11 +2,9 @@ package store.lkhun.demo.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import store.lkhun.demo.Service.CreateProductService;
+import store.lkhun.demo.Service.ProductService;
 import store.lkhun.demo.dto.ProductRequestDto;
 import store.lkhun.demo.dto.ResponseDto;
 
@@ -16,7 +14,12 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 public class RegisterProductController {
-    private final CreateProductService createProductService;
+    private final ProductService productService;
+
+    @GetMapping("/readAll")
+    public ResponseDto<?> readProducts() {
+        return productService.readProducts();
+    }
 
     @PostMapping(value = "/upload", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseDto<?> createProduct(@Valid @RequestPart(value = "requestDto") ProductRequestDto requestDto,
@@ -25,7 +28,23 @@ public class RegisterProductController {
         //org.springframework.web.multipart.support.StandardMultipartHttpServletRequest$StandardMultipartFile@681db269
         System.out.println(imageFile.getOriginalFilename());
         //aws.png
-        return createProductService.createProduct(requestDto, imageFile);
+        return productService.createProduct(requestDto, imageFile);
+    }
+
+    @PutMapping(value = "/update/{productName}")
+    public ResponseDto<?> updatePosts(@PathVariable(name = "productName") String productName,
+                                      @Valid @RequestPart(value = "requestDto") ProductRequestDto productRequestDto,
+                                      @RequestPart(value = "imageFile", required = false) MultipartFile imageFile) throws IOException{
+        System.out.println(imageFile);
+        //org.springframework.web.multipart.support.StandardMultipartHttpServletRequest$StandardMultipartFile@681db269
+        System.out.println(imageFile.getOriginalFilename());
+        //aws.png
+        return productService.updateProduct(productName, productRequestDto, imageFile);
+    }
+
+    @DeleteMapping("/delete/{productName}")
+    public ResponseDto<?> deleteProduct(@PathVariable (name = "productName") String productName) {
+        return productService.deleteProduct(productName);
     }
 
 }
