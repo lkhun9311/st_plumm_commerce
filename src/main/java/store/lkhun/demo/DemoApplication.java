eddb1cfc3ac24947f98e0ccd50528d0e5dc9bd03
lkhun9311 <lkhun9311@naver.com>
2023-06-07 15:29:03 +0900

    Add: 이미지 파일을 정상적으로 수신하고 AWS와 정상적으로 통신하도록 코드 작성

--- src/main/java/store/lkhun/demo/DemoApplication.java
+++ src/main/java/store/lkhun/demo/DemoApplication.java
@@ -2,12 +2,39 @@ package store.lkhun.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartProperties;
 
-@SpringBootApplication
+@EnableJpaAuditing
+@SpringBootApplication(
+		exclude = {
+				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+		}
+)
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(DemoApplication.class, args); }
+
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩 설정
+		multipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024); // 파일당 업로드 크기 제한 (5MB)
+		return multipartResolver;
+	}
+
+	@Bean(name = DispatcherServlet.MULTIPART_RESOLVER_BEAN_NAME)
+	public StandardServletMultipartResolver multipartResolver(MultipartProperties multipartProperties) {
+		StandardServletMultipartResolver multipartResolver = new StandardServletMultipartResolver();
+		multipartResolver.setStrictServletCompliance(true);
+		multipartResolver.setResolveLazily(multipartProperties.isResolveLazily());
+		return multipartResolver;
 	}
 
 }
