7ec37f9a315201ac93a537381ca56811c9714745
lkhun9311 <lkhun9311@naver.com>
2023-06-09 09:53:39 +0900

    Add: Amazon ElastiCache for Redis와 연동하기 위한 코드 작성

---
+++ src/main/java/store/lkhun/demo/config/RedisCacheConfig.java
@@ -0,0 +1,50 @@
+package store.lkhun.demo.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import store.lkhun.demo.global.CacheKey;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+@RequiredArgsConstructor
+public class RedisCacheConfig {
+
+    private final ObjectMapper objectMapper;
+
+    @Bean(name = "cacheManager")
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofSeconds(CacheKey.DEFAULT_EXPIRE_SEC))
+                .computePrefixWith(CacheKeyPrefix.simple())
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)));
+
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+
+        // Product
+        cacheConfigurations.put(CacheKey.PRODUCT, RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(CacheKey.PRODUCT_EXPIRE_SEC)));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(connectionFactory)
+                .cacheDefaults(configuration)
+                .withInitialCacheConfigurations(cacheConfigurations)
+                .build();
+    }
+}
---
+++ src/main/java/store/lkhun/demo/global/CacheKey.java
@@ -0,0 +1,8 @@
+package store.lkhun.demo.global;
+
+public class CacheKey {
+    private CacheKey() {}
+    public static final int DEFAULT_EXPIRE_SEC = 60;
+    public static final String PRODUCT = "product";
+    public static final int PRODUCT_EXPIRE_SEC = 30;
+}
