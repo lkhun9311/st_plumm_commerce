6ec0fbb93423090e989b6a66d8b1b77214ec1313
lkhun9311 <lkhun9311@naver.com>
2023-06-09 09:54:21 +0900

    Add: RegisterProductController 파일에 cache annotation 등록

--- src/main/java/store/lkhun/demo/controller/RegisterProductController.java
+++ src/main/java/store/lkhun/demo/controller/RegisterProductController.java
@@ -1,12 +1,15 @@
 package store.lkhun.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import store.lkhun.demo.Service.ProductService;
 import store.lkhun.demo.dto.ProductRequestDto;
 import store.lkhun.demo.dto.ResponseDto;
+import store.lkhun.demo.global.CacheKey;
 
 import javax.validation.Valid;
 import java.io.IOException;
@@ -16,11 +19,13 @@ import java.io.IOException;
 public class RegisterProductController {
     private final ProductService productService;
 
+    @CachePut(value = CacheKey.PRODUCT)
     @GetMapping("/readAll")
     public ResponseDto<?> readProducts() {
         return productService.readProducts();
     }
 
+    @CachePut(value = CacheKey.PRODUCT, key = "#requestDto.productName", unless = "#result == null")
     @PostMapping(value = "/upload", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseDto<?> createProduct(@Valid @RequestPart(value = "requestDto") ProductRequestDto requestDto,
                                          @RequestPart(value = "imageFile", required = false) MultipartFile imageFile) throws IOException {
@@ -31,6 +36,7 @@ public class RegisterProductController {
         return productService.createProduct(requestDto, imageFile);
     }
 
+    @CachePut(value = CacheKey.PRODUCT, key = "#productName", unless = "#result == null")
     @PutMapping(value = "/update/{productName}")
     public ResponseDto<?> updatePosts(@PathVariable(name = "productName") String productName,
                                       @Valid @RequestPart(value = "requestDto") ProductRequestDto productRequestDto,
@@ -42,6 +48,7 @@ public class RegisterProductController {
         return productService.updateProduct(productName, productRequestDto, imageFile);
     }
 
+    @CacheEvict(value = CacheKey.PRODUCT, key = "#productName")
     @DeleteMapping("/delete/{productName}")
     public ResponseDto<?> deleteProduct(@PathVariable (name = "productName") String productName) {
         return productService.deleteProduct(productName);
