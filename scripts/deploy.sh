7c2b113795b215215dbfe176d4b7edeacd790733
lkhun9311 <lkhun9311@naver.com>
2023-06-23 17:40:56 +0900

    Fix: Github Actions 재시도

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
-
-BUILD_JAR=$(ls /home/ec2-user/action/build/libs/demo-0.0.1-SNAPSHOT.jar)
+PROJECT_PATH="/home/ec2-user/app/42Partner-Backend"
+MODULE_NAME="module-api"
+CONTEXT="dev"
+JAR_PATH="$PROJECT_PATH/$MODULE_NAME/build/libs/*.jar"
+DEPLOY_PATH=$PROJECT_PATH/$MODULE_NAME
+DEPLOY_LOG_PATH="$PROJECT_PATH/$MODULE_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="$PROJECT_PATH/$MODULE_NAME/deploy_err.log"
+APPLICATION_LOG_PATH="$PROJECT_PATH/$MODULE_NAME/application.log"
+BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
 
-echo ">>> build 파일 복사" >> /home/ec2-user/action/deploy.log
-DEPLOY_PATH=/home/ec2-user/action/
+echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
+
+echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
+echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
+echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
+  echo "> 현재 동작중인 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
 else
-  echo ">>> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+  echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
+  echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
+  echo "> kill -9 $CURRENT_PID" >> $DEPLOY_LOG_PATH
+  kill -9 $CURRENT_PID
 fi
+# spring.profiles.active는 application.yml에 명시하고 빌드시에 이미 포함됨.
+DEPLOY_JAR="$DEPLOY_PATH/build/libs/$JAR_NAME"
+echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
+nohup java -jar $DEPLOY_JAR >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "$DEPLOY_JAR" >> /home/ec2-user/action/deploy.log
-echo ">>> DEPLOY_JAR 배포" >> /home/ec2-user/action/deploy.log
-nohup java -Dspring.profiles.active=dev -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+sleep 3
+
+echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
