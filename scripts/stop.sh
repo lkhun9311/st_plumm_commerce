36e1c46610910b0974db03877c058ac15f543d59
lkhun9311 <lkhun9311@naver.com>
2023-06-26 17:38:42 +0900

    Fix: Github Actions 재시도

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,9 +1,3 @@
 # demo-0.0.1-SNAPSHOT.jar 프로세스 ID(PID) 검색
 PID=$(ps -ef | grep 'demo-0.0.1-SNAPSHOT.jar' | grep -v grep | awk '{ print $2 }')
-
-if [ -z "$PID" ]; then
-    echo "demo-0.0.1-SNAPSHOT.jar가 실행되고 있지 않습니다."
-else
-    echo "demo-0.0.1-SNAPSHOT.jar가 실행 중인 PID: $PID. 이 프로세스를 종료합니다."
-    sudo kill -9 $PID
-fi
+sudo kill -9 $PID
